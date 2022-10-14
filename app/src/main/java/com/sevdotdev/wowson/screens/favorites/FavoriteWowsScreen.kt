2ca509b5afa3fe5d993911756142697c74ec8abf
DSeverns <dseverns@bignerdranch.com>
2022-10-14 08:36:50 -0400

    updated commons stuff for navigation

--- app/src/main/java/com/sevdotdev/wowson/screens/favorites/FavoriteWowsScreen.kt
+++ app/src/main/java/com/sevdotdev/wowson/screens/favorites/FavoriteWowsScreen.kt
@@ -3,6 +3,7 @@ package com.sevdotdev.wowson.screens.favorites
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Icon
@@ -11,13 +12,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.sevdotdev.wowson.R
 
 @Composable
 fun FavoritesScreen() {
-    Box(modifier = Modifier.fillMaxSize()){
-        Column(modifier = Modifier.align(Alignment.Center)) {
-            Icon(imageVector = Icons.Filled.Favorite, contentDescription = null)
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier.align(Alignment.Center), horizontalAlignment = Alignment.CenterHorizontally) {
+            Icon(imageVector = Icons.Filled.Favorite,
+                contentDescription = null,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .size(200.dp))
             Text(text = stringResource(id = R.string.favorites_screen_msg))
         }
     }
--- app/src/main/java/com/sevdotdev/wowson/screens/wowstats/WowStatsScreen.kt
+++ app/src/main/java/com/sevdotdev/wowson/screens/wowstats/WowStatsScreen.kt
@@ -1,34 +1,38 @@
 package com.sevdotdev.wowson.screens.wowstats
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.sevdotdev.domain.model.WowStats
 import com.sevdotdev.wowson.ui.common.core.icons.VectorIcons
 import com.sevdotdev.wowson.ui.common.core.icons.WowIcon
 
 @Composable
 fun WowStatsScreen(
-    stats: WowStats,
+    stats: ParcelableStats,
 ) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(3),
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp),
+            .padding(16.dp)
+            .background(MaterialTheme.colorScheme.surface),
     ) {
-        items((1..stats.indexOfWow).toList()) { item ->
+        items((1..stats.totalWowsInMovie).toList()) { item ->
             Icon(
                 imageVector = VectorIcons.WowIcon,
                 contentDescription = null,
-                tint = if (item == stats.indexOfWow) Color.Black else Color.Yellow,
+                tint = if (item == stats.indexOfWow) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onBackground,
+                modifier = Modifier.size(100.dp)
             )
         }
     }
